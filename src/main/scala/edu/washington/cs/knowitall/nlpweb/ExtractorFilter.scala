fc0c699d46ffa6851e6c528f3f8610bdb24b63a3
Michael Schmitz <michael@schmitztech.com>
2012-04-27 11:03:55 -0700

    Changed the format of extractions.

--- src/main/scala/edu/washington/cs/knowitall/nlpweb/ExtractorFilter.scala
+++ src/main/scala/edu/washington/cs/knowitall/nlpweb/ExtractorFilter.scala
@@ -76,18 +76,20 @@ class ExtractorFilter extends ToolFilter("extractor", List("reverb", "relnoun", 
       "<input name=\"check_"+extractor+"\" type=\"checkbox\" value=\"true\"" + (if (extractor == currentExtractor || params.get("check_" + extractor) == Some("true")) """checked="true" """ else "") + " /> "+extractor+"<br />"
     }).mkString("\n")
   }
-
+  
   override def doPost(params: Map[String, String]) = {
+	case class ExtractionSet(sentence: String, extractions: Seq[(String, Iterable[(Double, (String, String, String))])])
+	
     def chunk(string: String) = sentenceChunker.synchronized {
       sentenceDetector.sentDetect(string).map {
         string => new Sentence(sentenceChunker.chunkSentence(string), string)
       }
     }
     
-    def buildTable(extractions: (String, Iterable[(Double, (String, String, String))])) = {
-      "<table><tr><th colspan=\"4\">"+extractions._1 + " extractions"+"</th></tr>" + extractions._2.map { case (conf, (arg1, rel, arg2)) => 
+    def buildTable(set: ExtractionSet) = {
+      "<table><tr><th colspan=\"4\">" + set.sentence + "</th></tr>" + set.extractions.map(extractions => "<tr><th colspan=\"4\">"+extractions._1 + " extractions"+"</th></tr>" + extractions._2.map { case (conf, (arg1, rel, arg2)) => 
         "<tr><td>"+("%1.2f" format conf)+"</td><td>"+arg1+"</td><td>"+rel+"</td><td>"+arg2+"</td></tr>"
-      }.mkString("\n")+"</table><br/><br/>"
+      }.mkString("\n")).mkString("\n")+"</table><br/><br/>"
     }
 
     val extractorName = params("extractor")
@@ -101,9 +103,9 @@ class ExtractorFilter extends ToolFilter("extractor", List("reverb", "relnoun", 
       } yield (extrs)).toSeq.sortBy { case (extr, extrs) => this.tools.indexOf(extr) }
 
     val (chunkTime, chunked) = Timing.time(chunk(text))
-    val (extractionTime, extractions) = Timing.time(chunked.flatMap(extractor(_)))
+    val (extractionTime, extractions) = Timing.time(chunked.map(chunked => ExtractionSet(chunked.getTokensAsString, extractor(chunked))))
     ("chunking: " + Timing.Milliseconds.format(chunkTime) + "\n" +
       "extracting: " + Timing.Milliseconds.format(extractionTime),
-      "<p>" + extractions.map(_._2).flatten.size + " extraction(s):</p>" + extractions.map(buildTable(_)).mkString("\n"))
+      "<p>" + extractions.map(_.extractions).flatten.map(_._2).flatten.size + " extraction(s):</p>" + extractions.map(buildTable(_)).mkString("\n"))
   }
 }
