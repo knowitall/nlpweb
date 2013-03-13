bce3ce177f140bacc599540f9a1798a9b49a2773
Michael Schmitz <michael@schmitztech.com>
2013-03-13 15:23:35 -0700

    Use R2A2 and ReVerb from chunkedextractor.

--- src/main/scala/edu/knowitall/nlpweb/tool/ExtractorIntent.scala
+++ src/main/scala/edu/knowitall/nlpweb/tool/ExtractorIntent.scala
@@ -3,30 +3,26 @@ package nlpweb
 package tool
 
 import scala.Array.canBuildFrom
-import scala.collection.JavaConversions.iterableAsScalaIterable
+
 import common.Timing
-import edu.washington.cs.knowitall.argumentidentifier.ConfidenceMetric
-import edu.knowitall.chunkedextractor.{BinaryExtractionInstance, Nesty, Relnoun}
-import edu.washington.cs.knowitall.extractor.{R2A2, ReVerbExtractor}
-import edu.washington.cs.knowitall.extractor.conf.ReVerbOpenNlpConfFunction
-import edu.washington.cs.knowitall.nlp.ChunkedSentence
-import edu.washington.cs.knowitall.nlp.extraction.ChunkedBinaryExtraction
+import edu.knowitall.chunkedextractor.BinaryExtractionInstance
+import edu.knowitall.chunkedextractor.Nesty
+import edu.knowitall.chunkedextractor.R2A2
+import edu.knowitall.chunkedextractor.ReVerb
+import edu.knowitall.chunkedextractor.Relnoun
+import edu.knowitall.nlpweb.ToolIntent
 import edu.knowitall.ollie.Ollie
 import edu.knowitall.ollie.confidence.OllieConfidenceFunction
 import edu.knowitall.openparse
 import edu.knowitall.openparse.OpenParse
-import edu.knowitall.tool.chunk.{ChunkedToken, OpenNlpChunker}
-import edu.knowitall.tool.parse.MaltParser
-import edu.knowitall.tool.stem.{Lemmatized, MorphaStemmer}
+import edu.knowitall.tool.chunk.ChunkedToken
+import edu.knowitall.tool.stem.Lemmatized
+import edu.knowitall.tool.stem.MorphaStemmer
 import edu.knowitall.tool.tokenize.Token
-import edu.washington.cs.knowitall.util.DefaultObjects
-import unfiltered.request.HttpRequest
 import knowitall.srl.SrlExtractor
-import edu.knowitall.collection.immutable.Interval
 
 object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun", "nesty", "openparse", "ollie-clear", "ollie-malt", "srl")) {
   override val info = "Enter sentences from which to extract relations, one per line."
-  lazy val sentenceDetector = DefaultObjects.getDefaultSentenceDetector()
 
   lazy val ollieExtractor = new Ollie()
   lazy val ollieConfidence = OllieConfidenceFunction.loadDefaultClassifier()
@@ -34,15 +30,12 @@ object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun",
   lazy val openparseExtractor = {
     OpenParse.withDefaultModel()
   }
-  lazy val reverbExtractor = new ReVerbExtractor()
+  lazy val reverbExtractor = new ReVerb()
   lazy val nestyExtractor = new Nesty()
   lazy val r2a2Extractor = new R2A2()
   lazy val relnounExtractor = new Relnoun()
 
   lazy val srlExtractor = new SrlExtractor(SrlIntent.clearSrl)
-
-  lazy val reverbConfidence = new ReVerbOpenNlpConfFunction()
-  lazy val r2a2Confidence = new ConfidenceMetric()
 
   lazy val chunker = ChunkerIntent.opennlpChunker
 
@@ -50,13 +43,7 @@ object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun",
     lemmatized.token
   }
 
-  implicit def tokens2chunkedSentence(tokens: Seq[Lemmatized[ChunkedToken]]): ChunkedSentence = {
-    new ChunkedSentence(tokens.map(_.token.string).toArray, tokens.map(_.token.postag).toArray, tokens.map(_.token.chunk).toArray)
-  }
-
   def getExtractor(name: String): Seq[Lemmatized[ChunkedToken]] => List[(String, Iterable[Extraction])] = {
-    def triple(conf: Double)(extr: ChunkedBinaryExtraction): Extraction =
-      new Extraction(conf, extr.getArgument1.getText, extr.getRelation.getText, extr.getArgument2.getText)
     def tripleChunked(conf: Double)(inst: BinaryExtractionInstance[ChunkedToken]): Extraction = 
       new Extraction(conf, inst.extr.arg1.text, inst.extr.rel.text, inst.extr.arg2.text)
     def tripleOpenParse(conf: Double)(extr: openparse.extract.DetailedExtraction): Extraction =
@@ -76,13 +63,12 @@ object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun",
       }
       case "openparse" => openparseExtractor.extract(ParserIntent.maltParser.dependencyGraph(s.iterator.map(_.string).mkString(" "))).toSeq.map { case(conf, extr) => tripleOpenParse(conf)(extr) }
       case "reverb" =>
-        val extrs: List[ChunkedBinaryExtraction] = reverbExtractor.extract(s).toList
-        val confs: List[Double] = extrs map reverbConfidence.getConf
-        extrs.map{ extr => triple(reverbConfidence.getConf(extr))(extr) }
+        val extrs = reverbExtractor.extractWithConfidence(s.map(_.token)).toList
+        extrs.map{ case (conf, extr) => tripleChunked(conf)(extr) }
       case "nesty" => nestyExtractor.extract(s).map(ex => tripleChunked(0.0)(ex))
       case "r2a2" =>
-        val extrs: List[ChunkedBinaryExtraction] = r2a2Extractor.extract(s).toList
-        extrs.map{ extr => triple(r2a2Confidence.getConf(extr))(extr) }
+        val extrs = r2a2Extractor.extractWithConf(s.map(_.token)).toList
+        extrs.map{ case (Some(conf), extr) => tripleChunked(conf)(extr) case _ => throw new IllegalArgumentException("No confidence function for R2A2.")}
       case "relnoun" => relnounExtractor.extract(s).map(ex => tripleChunked(0.0)(ex))
       case "srl" => {
         val parser = ParserIntent.clearParser
@@ -106,15 +92,15 @@ object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun",
     case class ExtractionSet(sentence: String, extractions: Seq[(String, Iterable[Extraction])])
 
     def chunk(string: String) = chunker.synchronized {
-      sentenceDetector.sentDetect(string).map {
-        string => chunker.chunk(string) map MorphaStemmer.lemmatizeToken
+      string.split("\n").map {
+        string => chunker.chunk(string.trim) map MorphaStemmer.lemmatizeToken
       }
     }
 
     def buildTable(set: ExtractionSet) = {
       "<table><tr><th colspan=\"4\">" + set.sentence + "</th></tr>" + set.extractions.map(extractions => "<tr><th colspan=\"4\">" + extractions._1 + " extractions" + "</th></tr>" + extractions._2.map {
         case Extraction(conf, _, arg1, rel, arg2) =>
-          "<tr><td>" + ("%1.2f" format conf) + "</td><td>" + arg1 + "</td><td>" + rel + "</td><td>" + arg2 + "</td></tr>"
+          "<tr><td>" + ("%1.2f" format conf) + "</td><td>" + arg1.string + "</td><td>" + rel.string + "</td><td>" + arg2.string + "</td></tr>"
       }.mkString("\n")).mkString("\n") + "</table><br/><br/>"
     }
 
@@ -128,7 +114,7 @@ object ExtractorIntent extends ToolIntent("extractor", List("reverb", "relnoun",
       } yield (extrs)).toSeq.sortBy { case (extr, extrs) => this.tools.indexOf(extr) }
 
     val (chunkTime, chunked) = Timing.time(chunk(text))
-    val (extractionTime, extractions) = Timing.time(chunked.map(chunked => ExtractionSet(chunked.getTokensAsString, extractor(chunked))))
+    val (extractionTime, extractions) = Timing.time(chunked.map(chunked => ExtractionSet(chunked.map(_.token).mkString(" "), extractor(chunked))))
     ("chunking: " + Timing.Milliseconds.format(chunkTime) + "\n" +
       "extracting: " + Timing.Milliseconds.format(extractionTime),
       "<p>" + extractions.map(_.extractions).flatten.map(_._2).flatten.size + " extraction(s):</p>" + extractions.map(buildTable(_)).mkString("\n"))
