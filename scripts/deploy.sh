a2dde50237397debce136aa10be76b62498670b2
Michael Schmitz <schmmd@cs.washington.edu>
2013-04-04 14:47:36 -0700

    Add scripts to nlpweb.

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Compile the new source into an assembly
+sbt clean compile assembly
+
+cp target/nlpweb-assembly*jar nlpweb.jar
+
+rc=$?
+if [[ $rc == 0 ]] ; then
+  ./start.sh
+  rc=$?
+  if [[ $rc == 0 ]] ; then
+    echo "Try it out: http://nlpweb.cs.washington.edu" | mail -s "Automatic Notification: NlpWeb Updated" schmmd@cs.washington.edu
+  else
+    echo "Could not start NlpWeb on Reliable." | mail -s "Automatic Notification: NlpWeb Start Failed" schmmd@cs.washington.edu
+  fi
+else
+  echo "Could not compile NlpWeb on Reliable." | mail -s "Automatic Notification: NlpWeb Build Failed" schmmd@cs.washington.edu
+fi
---
+++ scripts/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+startCommand='java -Xms1g -Xmx8g -jar nlpweb.jar --port 8040'
+
+demoPid=$(<nlpweb.pid)
+if [ $? -eq 0 ]; then
+    echo "killing previous instance: $demoPid"
+    kill $demoPid
+    echo "sleeping for 2 seconds"
+    sleep 2
+fi
+
+echo "running start command: $startCommand"
+nohup $startCommand > nlpweb.out 2> nlpweb.err &
+ec=$?
+
+sleep 2
+
+ps -p $! > /dev/null
+if [ $? -eq 0 ]; then
+    echo $! > nlpweb.pid
+    echo "pid: $!"
+else
+    echo "error: nlpweb failed to start!"
+    exit 1
+fi
+
+exit $ec
